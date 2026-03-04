b1e307eb84ea048bf458b845b78b1577d77e2ac7
Paulo Córdova <control@pcgoperacion.com>
2026-03-04 18:10:20 +0000

    al firmar: ## Error Type Runtime FirebaseError
    ## Error Message
    Firebas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:118089876893:web:ae3e5cfefe43be8fc2db74",
   "apiKey": "AIzaSyBExzdbk96pF1qW0uXwIRuJi7rgr1xvQ9M",
   "authDomain": "studio-1862261800-55e9f.firebaseapp.com",
+  "storageBucket": "studio-1862261800-55e9f.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "118089876893"
 };
